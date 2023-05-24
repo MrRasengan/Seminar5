16b6c3b5558ff522389cd346a78e2e23e0c7d62f
Konstantin <kostik-kostik-12@mail.ru>
2023-05-24 13:24:22 +0300

    add Leason1

---
+++ Leason2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 32: Напишите программу замена элементов массива: 
+//положительные элементы замените на соответствующие отрицательные, 
+//и наоборот.
+//[-4, -8, 8, 2] -> [4, 8, -8, -2] 
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+
+void GetValue(int[] userArray)
+{
+    int arrSize = userArray.Length;
+    for(int el = 0; el < arrSize; el++)
+    {
+        userArray[el] *= -1;
+        Console.Write($"{userArray[el]} ");
+    }
+}
+
+int[] array = GetArray(4, -9 , 9);
+Console.WriteLine();
+GetValue(array);
---
+++ Leason3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу которая проверяет наличие числа в массиве.
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+        Console.Write($"{res[i]} ");
+    }
+    return res;
+}
+
+void CheckNumArray(int[] arr, int num)
+{
+    bool check = false;
+    num = Math.Abs(num);
+    foreach(int el in arr)
+    {
+        if (el == num)
+        {
+            Console.WriteLine($"Число {num} есть в массиве {String.Join(" ",arr)} ");
+            check = true;
+            break;
+        }
+    }
+        if (check == false)
+        {
+            Console.WriteLine($"Числа {num} нет в массиве {String.Join(" ",arr)} ");
+        }
+}
+
+int[] array = GetArray(7, -10, 10);
+Console.WriteLine();
+CheckNumArray(array, 4);
